91df7141c8989317219536bfff59c7f494d99cbc
Kiran H <kiranhari1998@gmail.com>
2023-09-02 00:45:45 +0530

    logic for retriving identity request

--- api/apihandler.go
+++ api/apihandler.go
@@ -10,18 +10,8 @@ import (
 )
 
 type API struct {
-	DB *db.DataBase
-}
-
-type UserSwaggo struct {
-	ID             int        `db:"id" json:"id"`
-	PhoneNumber    string     `db:"phone_number" json:"phone_number"`
-	Email          string     `db:"email" json:"email"`
-	LinkedID       *int       `db:"linked_id" json:"linked_id"`
-	LinkPrecedence string     `db:"link_precedence" json:"link_precedence"`
-	CreatedAt      time.Time  `db:"created_at" json:"created_at"`
-	UpdatedAt      time.Time  `db:"updated_at" json:"updated_at"`
-	DeletedAt      *time.Time `db:"deleted_at" json:"deleted_at"`
+	DB  *db.DataBase
+	log *log.Logger
 }
 
 // CreateUser creates a new user.
@@ -29,8 +19,8 @@ type UserSwaggo struct {
 // @Description Create a new user with the provided data
 // @Accept json
 // @Produce json
-// @Param user body UserSwaggo true "User object"
-// @Success 200 {object} UserSwaggo
+// @Param user body model.UserSwaggo true "User object"
+// @Success 200 {object} model.UserSwaggo
 // @Router /users/create [post]
 func (api *API) CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user model.User
@@ -63,7 +53,7 @@ func (api *API) CreateUser(w http.ResponseWriter, r *http.Request) {
 // @Description Update an existing user with the provided data
 // @Accept json
 // @Produce json
-// @Param user body UserSwaggo true "User object"
+// @Param user body model.UserSwaggo true "User object"
 // @Success 200
 // @Router /users/update [put]
 func (api *API) UpdateUser(w http.ResponseWriter, r *http.Request) {
@@ -88,54 +78,89 @@ func (api *API) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-type IdentiteyRequest struct {
-	Email       string `json:"email"`
-	PhoneNumber string `json:"phoneNumber"`
-}
-
-type IdentityResponse struct {
-	Contact struct {
-		PrimaryContactID    int      `json:"primaryContactId"`
-		Emails              []string `json:"emails"`
-		PhoneNumbers        []string `json:"phoneNumbers"`
-		SecondaryContactIDs []int    `json:"secondaryContactIds"`
-	} `json:"contact"`
-}
-
 // Identity Return user data
 // @Summary Return user data
 // @Description This endpoint is used to return data related to email or phone number supplied
 // @Accept json
 // @Produce json
-// @Param user body IdentiteyRequest true "enter email and phone number"
-// @Success 200 {object} IdentityResponse
+// @Param user body model.IdentityRequest true "enter email and phone number"
+// @Success 200 {object} model.IdentityResponse
 // @Router /users/create [post]
 func (api *API) Identity(w http.ResponseWriter, r *http.Request) {
+	// Parse the JSON request
+	var request model.IdentityRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		api.log.Fatal("Error occured while parsing input request")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
+	//get users from db based on identity request in put
+
+	users, err := api.DB.GetUsers(request)
+	if err != nil {
+		api.log.Fatal("error occured while fetching details drom DB", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if len(users) == 0 {
+		api.log.Println("No entries in DB for the given details")
+		api.log.Println("Creating entry in to the database")
+		//todo addusers
+	}
+
+	contact := &model.Contact{
+		PrimaryContactID:    getPrimaryID(users),
+		Emails:              getEmails(users),
+		PhoneNumbers:        getPhoneNumbers(users),
+		SecondaryContactIDs: getSecondaryContactIDs(users),
+	}
+
+	identityResp := &model.IdentityResponse{
+		Contact: *contact,
+	}
+
+	// Encode and send the JSON response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(identityResp); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
-/* func (api *API) ReadUsers(w http.ResponseWriter, r *http.Request) {
-	users, err := api.DB.GetUsers()
-	if err != nil {
-		log.Printf("failed to get users: %v", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+func getEmails(users []model.User) []string {
+	var emails []string
+	for _, user := range users {
+		emails = append(emails, user.Email)
 	}
+	return emails
+}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(users)
-} */
-
-/* func (api *API) DeleteUser(w http.ResponseWriter, r *http.Request) {
-	// Parse the ID from the URL path
-	id := r.URL.Path[len("/users/delete/"):]
-
-	err := api.DB.DeleteUser(id)
-	if err != nil {
-		log.Printf("failed to delete user: %v", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+func getPhoneNumbers(users []model.User) []string {
+	var phoneNumbers []string
+	for _, user := range users {
+		phoneNumbers = append(phoneNumbers, user.PhoneNumber)
 	}
+	return phoneNumbers
+}
 
-	w.WriteHeader(http.StatusOK)
-} */
+func getSecondaryContactIDs(users []model.User) []int {
+	var secondaryContactIDs []int
+	for _, user := range users {
+		if user.LinkPrecedence == "secondary" {
+			secondaryContactIDs = append(secondaryContactIDs, user.ID)
+		}
+	}
+	return secondaryContactIDs
+}
+
+func getPrimaryID(users []model.User) int {
+	var primaryID int
+	for _, user := range users {
+		if user.LinkPrecedence == "primary" {
+			primaryID = user.ID
+		}
+	}
+	return primaryID
+}
