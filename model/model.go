9a8dc93788d6110d2d08238ca1d2d9e2a673ee06
Kiran H <kiranhari1998@gmail.com>
2023-09-02 00:46:21 +0530

    inlcude identioty req and responce models

--- model/model.go
+++ model/model.go
@@ -13,3 +13,30 @@ type User struct {
 	UpdatedAt      time.Time  `db:"updated_at" json:"updated_at"`
 	DeletedAt      *time.Time `db:"deleted_at" json:"deleted_at"`
 }
+
+type IdentityRequest struct {
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phoneNumber"`
+}
+
+type IdentityResponse struct {
+	Contact Contact `json:"contact"`
+}
+
+type Contact struct {
+	PrimaryContactID    int      `json:"primaryContactId"`
+	Emails              []string `json:"emails"`
+	PhoneNumbers        []string `json:"phoneNumbers"`
+	SecondaryContactIDs []int    `json:"secondaryContactIds"`
+}
+
+type UserSwaggo struct {
+	ID             int        `db:"id" json:"id"`
+	PhoneNumber    string     `db:"phone_number" json:"phone_number"`
+	Email          string     `db:"email" json:"email"`
+	LinkedID       *int       `db:"linked_id" json:"linked_id"`
+	LinkPrecedence string     `db:"link_precedence" json:"link_precedence"`
+	CreatedAt      time.Time  `db:"created_at" json:"created_at"`
+	UpdatedAt      time.Time  `db:"updated_at" json:"updated_at"`
+	DeletedAt      *time.Time `db:"deleted_at" json:"deleted_at"`
+}
