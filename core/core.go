c077189be1831a20997152ad9a06a5a82e767c92
Kiran H <kiranhari1998@gmail.com>
2023-09-01 22:23:38 +0530

    API endpoint registering

--- core/core.go
+++ core/core.go
@@ -6,35 +6,54 @@ import (
 	"identityreconciliation/model"
 	"log"
 	"net/http"
+	"os"
 
-	httpSwagger "github.com/swaggo/http-swagger"
+	"github.com/gorilla/mux"
 )
 
 type Core struct {
 	DB  *db.DataBase
 	API *api.API
+	log *log.Logger
+}
+
+func NewCoreService(db *db.DataBase, apiHandler *api.API) *Core {
+	return &Core{
+		DB:  db,
+		API: apiHandler,
+		log: log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile),
+	}
+}
+
+func (c *Core) CallRun() {
+	c.run()
 }
 
 func (c *Core) run() {
+
+	c.log.Println("starting core")
+	r := mux.NewRouter()
+
 	// Configure database connection
-	if err := c.DB.InitDB("mydb.sqlite"); err != nil {
-		log.Fatalf("failed to initialize database: %v", err)
+	if err := c.DB.InitDB("identity.db"); err != nil {
+		c.log.Fatalf("failed to initialize database: %v", err)
 	}
 	defer c.DB.CloseDB()
 
 	// Create tables based on struct definitions
 	if err := c.DB.CreateTable(model.User{}); err != nil {
-		log.Fatalf("failed to create table: %v", err)
+		c.log.Fatalf("failed to create table: %v", err)
 	}
 
-	// Register API handlers
-	//http.HandleFunc("/users", c.API.ReadUsers)
-	http.HandleFunc("/users/create", c.API.CreateUser)
-	http.HandleFunc("/users/update", c.API.UpdateUser)
-	//http.HandleFunc("/users/delete", c.API.DeleteUser)
+	// Register API handlers using gorilla/mux
+	//r.HandleFunc("/users", c.API.ReadUsers).Methods("GET")
+	r.HandleFunc("/users/create", c.API.CreateUser).Methods("POST")
+	r.HandleFunc("/users/update", c.API.UpdateUser).Methods("PUT")
+	//r.HandleFunc("/users/delete", c.API.DeleteUser).Methods("DELETE")
+	r.HandleFunc("/identity", c.API.Identity).Methods("POST")
 
-	// Serve the Swagger UI at /swagger/index.html
-	http.Handle("/swagger/", httpSwagger.Handler())
+	// Use the gorilla/mux router
+	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
