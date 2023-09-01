36b9e65fbef06e06b0001a27988d85435fe1b560
Kiran H <kiranhari1998@gmail.com>
2023-09-01 22:21:37 +0530

    updated main.go create new core instance

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"identityreconciliation/api"
+	"identityreconciliation/core"
+	db "identityreconciliation/database"
+)
+
+func main() {
+	db := &db.DataBase{}
+	apiHandler := &api.API{}
+
+	newCoreService := core.NewCoreService(db, apiHandler)
+	newCoreService.CallRun()
+}
