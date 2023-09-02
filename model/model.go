412490e6c9711b9f5a9e1a99a5b4e010b26bb525
Kiran H <kiranhari1998@gmail.com>
2023-09-02 22:27:34 +0530

    update user model to handle nullint from db

--- model/model.go
+++ model/model.go
@@ -1,17 +1,20 @@
 package model
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 // Struct to define the columns for the User Details Table
 type User struct {
-	ID             int       `db:"id" json:"id"`
-	PhoneNumber    string    `db:"phone_number" json:"phone_number"`
-	Email          string    `db:"email" json:"email"`
-	LinkedID       int       `db:"linked_id" json:"linked_id"`
-	LinkPrecedence string    `db:"link_precedence" json:"link_precedence"`
-	CreatedAt      time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt      time.Time `db:"updated_at" json:"updated_at"`
-	DeletedAt      time.Time `db:"deleted_at" json:"deleted_at"`
+	ID             int           `db:"id" json:"id"`
+	PhoneNumber    string        `db:"phone_number" json:"phone_number"`
+	Email          string        `db:"email" json:"email"`
+	LinkedID       sql.NullInt64 `db:"linked_id" json:"linked_id"`
+	LinkPrecedence string        `db:"link_precedence" json:"link_precedence"`
+	CreatedAt      time.Time     `db:"created_at" json:"created_at"`
+	UpdatedAt      time.Time     `db:"updated_at" json:"updated_at"`
+	DeletedAt      time.Time     `db:"deleted_at" json:"deleted_at"`
 }
 
 type IdentityRequest struct {
