1eac49b6ced097143551c25afd2a52bfdddf057a
Kiran H <kiranhari1998@gmail.com>
2023-09-02 22:27:04 +0530

    initialise storage and service modules

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"identityreconciliation/core"
 	"identityreconciliation/repository"
 	"identityreconciliation/service"
 )
 
 func main() {
-	storage := &repository.Repository{}
-	service := &service.Service{}
-
+	storage := repository.NewRepository(&sql.DB{})
+	service := service.NewService(storage)
 	newCoreService := core.NewCoreService(storage, service)
+
 	newCoreService.CallRun()
 }
