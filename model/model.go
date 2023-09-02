a4751bdb50278e5ee462461a54179da02b4e2d57
Kiran H <kiranhari1998@gmail.com>
2023-09-02 23:19:54 +0530

    handle sql nulltimes in user model

--- model/model.go
+++ model/model.go
@@ -14,7 +14,7 @@ type User struct {
 	LinkPrecedence string        `db:"link_precedence" json:"link_precedence"`
 	CreatedAt      time.Time     `db:"created_at" json:"created_at"`
 	UpdatedAt      time.Time     `db:"updated_at" json:"updated_at"`
-	DeletedAt      time.Time     `db:"deleted_at" json:"deleted_at"`
+	DeletedAt      sql.NullTime  `db:"deleted_at" json:"deleted_at"`
 }
 
 type IdentityRequest struct {
