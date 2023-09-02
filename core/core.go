b8d2f1cbf2e81f4b11dc54df0bd37ed4fa73dc42
Kiran H <kiranhari1998@gmail.com>
2023-09-02 14:32:20 +0530

    updated core

--- core/core.go
+++ core/core.go
@@ -1,9 +1,9 @@
 package core
 
 import (
-	"identityreconciliation/api"
-	db "identityreconciliation/database"
 	"identityreconciliation/model"
+	"identityreconciliation/repository"
+	"identityreconciliation/service"
 	"log"
 	"net"
 	"net/http"
@@ -16,21 +16,22 @@ import (
 )
 
 type Core struct {
-	DB  *db.DataBase
-	API *api.API
-	log *log.Logger
+	storage *repository.Repository
+	service *service.Service
+	log     *log.Logger
 }
 
-func NewCoreService(db *db.DataBase, apiHandler *api.API) *Core {
+func NewCoreService(storage *repository.Repository, service *service.Service) *Core {
 	return &Core{
-		DB:  db,
-		API: apiHandler,
-		log: log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile),
+		storage: storage,
+		service: service,
+		log:     log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile),
 	}
 }
 
 func (c *Core) CallRun() {
 	c.run()
+	//api.NewService(c.DB)
 }
 
 func (c *Core) run() {
@@ -39,27 +40,27 @@ func (c *Core) run() {
 	router := mux.NewRouter()
 
 	// Configure database connection
-	if err := c.DB.InitDB("identity.db"); err != nil {
+	if err := c.storage.InitDB("identity.db"); err != nil {
 		c.log.Fatalf("failed to initialize database: %v", err)
 	}
-	defer c.DB.CloseDB()
+	defer c.storage.CloseDB()
 
 	// Create tables based on struct definitions
-	if err := c.DB.CreateTable(model.User{}); err != nil {
+	if err := c.storage.CreateTable(model.User{}); err != nil {
 		c.log.Fatalf("failed to create table: %v", err)
 	}
 
 	corsHandler := handlers.CORS(
-		handlers.AllowedOrigins([]string{"http://localhost:3000"}),
+		handlers.AllowedOrigins([]string{"http://localhost:8080"}),
 		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
 		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
 	)
 	handler := c.corsMiddleware(router)
 
 	// Register API handlers using gorilla/mux
-	router.HandleFunc("/users/create", c.API.CreateUser).Methods("POST")
-	router.HandleFunc("/users/update", c.API.UpdateUser).Methods("PUT")
-	router.HandleFunc("/identity", c.API.Identify).Methods("POST")
+	//router.HandleFunc("/users/create", c.API.CreateUser).Methods("POST")
+	//router.HandleFunc("/users/update", c.API.UpdateUser).Methods("PUT")
+	router.HandleFunc("/identify", c.service.Identify).Methods("POST")
 
 	//enable swagger
 	c.EnableSwagger(c.getURL(), router)
@@ -87,7 +88,7 @@ func (c *Core) getURL() string {
 	return url
 }
 
-func (c *Core) EnableSwagger(url string, r *mux.Router) {
+func (c *Core) EnableSwagger(url string, router *mux.Router) {
 
 	/* r.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
 	httpSwagger.URL(url+"/swagger/doc.json"),
@@ -96,29 +97,22 @@ func (c *Core) EnableSwagger(url string, r *mux.Router) {
 	httpSwagger.DomID("swagger-ui"))).Methods(http.MethodGet) */
 
 	swaggerURL := "/docs/swagger.json"
-	r.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
+	router.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
 		httpSwagger.URL(swaggerURL), // The url pointing to API definition
 	))
 	// router.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
 	// 	httpSwagger.URL(swaggerURL), // The url pointing to API definition
 	// ))
-	r.PathPrefix("/docs/").Handler(http.StripPrefix("/docs/", http.FileServer(http.Dir("./docs"))))
+	router.PathPrefix("/docs/").Handler(http.StripPrefix("/docs/", http.FileServer(http.Dir("./docs"))))
 	// Log URLs
-	log.Println("Swagger UI (API Documentation): http://localhost:8080/swagger/")
-	log.Println("Swagger JSON Specification: http://localhost:8080/docs" + swaggerURL)
+	c.log.Println("Swagger UI (API Documentation): http://localhost:8080/swagger/")
+	c.log.Println("Swagger JSON Specification: http://localhost:8080/docs" + swaggerURL)
 }
-
-/* func (c *Core) EnableSwagger(r *mux.Router) {
-	// Serve the Swagger JSON file from the "./docs" directory
-	r.PathPrefix("/swagger/").Handler(http.StripPrefix("/swagger/", http.FileServer(http.Dir("./docs"))))
-
-	// Optionally, you can configure the Swagger UI settings here
-} */
 
 // corsMiddleware is a middleware function to set the CORS headers in the response.
 func (c *Core) corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Set the Access-Control-Allow-Origin header to allow requests from http://localhost:3000
+		// Set the Access-Control-Allow-Origin header to allow requests from http://localhost:8080
 		//	for key, values := range r.Header {
 		//		log.Printf("%s: %v\n", key, values)
 		//	}
